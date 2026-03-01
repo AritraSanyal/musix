9555b0127b3e2ec160485ec30d5d4b068574eb83
Granthik Som <granthiksom@gmail.com>
2026-03-01 17:16:31 +0530

    "okey"

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/bottombar.dart';
 import 'package:flutter_application_1/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -18,92 +19,99 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('S E T T I N G S')),
-      body: Column(
+      body: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Dark Mode',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                //switch
-                CupertinoSwitch(
-                  value: Provider.of<ThemeProvider>(
-                    context,
-                    listen: false,
-                  ).isDarkMode,
-                  onChanged: (value) => Provider.of<ThemeProvider>(
-                    context,
-                    listen: false,
-                  ).toggleTheme(),
-                ),
-              ],
-            ),
-          ),
-
-          GestureDetector(
-            onTap: () {
-              openWebsite();
-            },
-            child: Container(
-              height: 80,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'ABOUT ',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      textBaseline: TextBaseline.alphabetic,
-                      fontSize: 20,
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Dark Mode',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                  ),
-                ],
+                    //switch
+                    CupertinoSwitch(
+                      value: Provider.of<ThemeProvider>(
+                        context,
+                        listen: false,
+                      ).isDarkMode,
+                      onChanged: (value) => Provider.of<ThemeProvider>(
+                        context,
+                        listen: false,
+                      ).toggleTheme(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ),
-          Container(
-            height: 80,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Log Out',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    textBaseline: TextBaseline.alphabetic,
-                    fontSize: 20,
-                    color: Colors.red,
+
+              GestureDetector(
+                onTap: () {
+                  openWebsite();
+                },
+                child: Container(
+                  height: 80,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Theme.of(
+                      context,
+                    ).colorScheme.primary.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'ABOUT ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          textBaseline: TextBaseline.alphabetic,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+              Container(
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Log Out',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        textBaseline: TextBaseline.alphabetic,
+                        fontSize: 20,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
-      bottomNavigationBar: const BottomAppBar(),
+      bottomNavigationBar: const Bottombar(),
     );
   }
 }
