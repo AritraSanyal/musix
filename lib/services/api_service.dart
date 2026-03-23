f518ea3ae8f73d93d528e8835799f7fbcc5083b9
Aritra Sanyal <aritras2003@gmail.com>
2026-03-24 02:36:39 +0530

    fix: update ApiService baseUrl to use local IP for device testing
    - Changed baseUrl from localhost to 10.95.65.227:8080
    - Required for Flutter app on device to connect to backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'http://10.95.65.227:8080';
   static String? _token;
 
   static void setToken(String token) {
